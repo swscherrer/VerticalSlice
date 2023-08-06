e6b8ee0556384aa62a268e754bd40f51814940e7
Steve Scherrer <steve_scherrer@yahoo.com>
2023-08-05 23:29:37 -0600

    Initial creation

---
+++ src/VerticalSlice.Api/SomeSlice/Startup.cs
@@ -0,0 +1,10 @@
+﻿namespace VerticalSlice.Api.SomeSlice
+{
+    public static class Startup
+    {
+        public static WebApplication? AddSomeSliceRouting(this WebApplication? webApp)
+        {
+            return webApp;
+        }
+    }
+}
---
+++ wiki/Home.md
@@ -0,0 +1,7 @@
+# Home
+
+General home description.
+
+## SomeSlice
+
+Something about the Slice.
